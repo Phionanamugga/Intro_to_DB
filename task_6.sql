2678a5ef6ea18704a4250b7a981121f7c6c41dbc
Phionanamugga <phionanamugga15@gmail.com>
2025-07-06 22:45:44 +0200

    creates sql file

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,10 @@
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- task_6.sql
+-- Script to insert multiple rows into the customer table in alx_book_store database
+
+-- task_6.sql
+-- Script to insert multiple rows into the customer table in alx_book_store database
+
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
